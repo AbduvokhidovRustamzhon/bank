9ed81f94d30c3d5592695fc7fec43aeee9250840
Rustam <abduvokhidov.rb@phystech.edu>
2021-04-06 11:26:22 +0500

    initial version

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -1,10 +1,25 @@
 package types
 
+//Money - представялет собой денежную сумму в минимальных единицах (центы, копейки, дирамы и т.д)
 type Money int64
+
 type Category string
 
-type Payment struct {
-	ID        int
-	Amount    Money
+
+// новый Status представляет собой статус платежа
+type Status string
+
+
+const (
+	StatusOk Status = "OK"
+	StatusFail 	Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"
+)
+
+// new Payment - представляет информацию о платеже.
+type Payment struct{
+	ID int
+	Amount Money
 	Category Category
+	Status Status
 }
